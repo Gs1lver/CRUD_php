fba7e0765c069a0aec3b33a234dea7649ba5817e
Gs1lver <alebasiwork@gmail.com>
2023-09-19 23:19:15 -0300

    exclusão funcionando

--- consulta.php
+++ consulta.php
@@ -26,37 +26,53 @@
 </body>
 </html>
 <?php
+include('database.php');
 
-    include('database.php');
+if (isset($_GET["consulta"])) {
+    try {
+        $nome = $_GET["consulta"];
+        $smt = consultar($nome);
+        echo "<form method='post'><table border='1px'>";
+        echo "<tr><th></th><th>Nome</th><th>Preço</th><th>Categoria</th><th>Foto</th></tr>";
 
-    if (isset($_GET["consulta"])){
-        try{
-            $nome = $_GET["consulta"];
-            $smt = consultar($nome);
-            echo "<form method='post'><table border='1px'>";
-            echo "<tr><th></th><th>Nome</th><th>Preço</th><th>Categoria</th><th>Foto</th></tr>";
-
-            while ($row = $smt->fetch()){
-                echo "<tr>";
-                echo "<td><input type='radio' name='nomeProduto'>";
-                echo "<td>{$row['nome']}</td>";
-                echo "<td>{$row['preco']}</td>";
-                echo "<td>{$row['categoria']}</td>";
-                if ($row["foto"] != null)
-                    echo "<td><img src='{$row['foto']}' width='50px' height='50px'></td>";
-                else
-                    echo "<td>-</td>";
-                echo "</tr>";
-            }
-
-            echo "</table><br>";
-            echo "<input type='submit' value='Excluir' class='button'></input>";
-            echo "<input type='submit' value='Alterar' class='button'></input>";
-            echo "</form>";
-            
-        } catch (PDOException $e){
-            echo "Erro ao consultar produto: " . $e->getMessage();
+        while ($row = $smt->fetch()) {
+            $nomeProduto = $row['nome']; // Substitua 'id' pelo nome do seu campo ID
+            echo "<tr>";
+            echo "<td><input type='radio' name='nomeProduto' value='$nomeProduto'></td>"; // Use o ID como valor
+            echo "<td>{$row['nome']}</td>";
+            echo "<td>{$row['preco']}</td>";
+            echo "<td>{$row['categoria']}</td>";
+            if ($row["foto"] != null)
+                echo "<td><img src='{$row['foto']}' width='50px' height='50px'></td>";
+            else
+                echo "<td>-</td>";
+            echo "</tr>";
         }
-    }
 
+        echo "</table><br>";
+        echo "<input type='submit' name='excluir' value='Excluir' class='button'></input>";
+        echo "<input type='submit' value='Alterar' class='button'></input>";
+        echo "</form>";
+
+        //exclusão
+        if (isset($_POST["excluir"]) && isset($_POST["nomeProduto"])) {
+            $produtoExcluir = $_POST["nomeProduto"];
+            excluir($produtoExcluir);
+
+        } elseif (isset($_POST["excluir"])) {
+            echo "<span>Selecione um produto para excluir!</span>";
+        }
+
+        //alteração
+        if (isset($_POST["alterar"]) && isset($_POST["nomeProduto"])) {
+            $produtoAlterar = $_POST["nomeProduto"];
+            header("Location: alterar.php?produto=$produtoAlterar");
+        } elseif (isset($_POST["alterar"])) {
+            echo "<span>Selecione um produto para alterar!</span>";
+        }
+
+    } catch (PDOException $e) {
+        echo "Erro ao consultar produto: " . $e->getMessage();
+    }
+}
 ?>
--- database.php
+++ database.php
@@ -18,27 +18,34 @@
 
     function cadastrar($nome, $preco, $categoria, $foto){
         try{
+            $nome = strtolower($nome);
             $pdo = conexao();
-            //$rows = verificarProduto();
+            $rows = verificarProdutoExistente($nome, $pdo);
 
-            //if ($rows <= 0){
+            if ($rows <= 0){
                 $smt = $pdo->prepare("INSERT INTO padaria (nome, preco, categoria, foto) VALUES (:nome, :preco, :categoria, :foto)");
                 $smt->bindParam(':nome', $nome);
                 $smt->bindParam(':preco', $preco);
                 $smt->bindParam(':categoria', $categoria);
                 $smt->bindParam(':foto', $foto);
                 $smt->execute();
-
                 echo "<span>Produto cadastrado com sucesso!</span>";
-            //}
+            } else {
+                echo "<span>Produto já cadastrado!</span>";
+            }
 
         } catch (PDOException $e){
             echo "Erro ao cadastrar produto: " . $e->getMessage();
         }
     }
 
-    function verificarProduto(){
-
+    function verificarProdutoExistente($nome, $pdo){
+        $nome = strtolower($nome);
+        $smt = $pdo->prepare("SELECT * FROM padaria WHERE nome = :nome");
+        $smt->bindParam(':nome', $nome);
+        $smt->execute();
+        $rows = $smt->rowCount();
+        return $rows;
     }
     
     function consultar($nome = null) {
@@ -54,8 +61,16 @@
         return $smt;
     }
 
-    function excluir(){
-
+    function excluir($nome){
+        try{
+            $pdo = conexao();
+            $smt = $pdo->prepare("DELETE FROM padaria WHERE nome = :nome");
+            $smt->bindParam(':nome', $nome);
+            $smt->execute();
+            echo "<span>Produto excluído com sucesso! Consulte novamente para atualizar a tabela!</span>";
+        } catch (PDOException $e){
+            echo "Erro ao excluir produto: " . $e->getMessage();
+        }
     }
 
     function alterar(){
